1f3a6d04738291a84ff6952cd134bde08a5c8a0e
Jan Kennu Paz <jankennupaz@hotmail.com>
2022-03-03 19:57:58 +0800

    setup app and dependency injection

---
+++ app/src/main/java/com/ausom/findr/di/ExecutorModule.kt
@@ -0,0 +1,17 @@
+package com.ausom.findr.di
+
+import com.ausom.domain.abstraction.PostExecutionThread
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+interface ExecutorModule {
+
+    @get:[Binds Singleton]
+    val PostExecutionThreadImpl.postExecutionThread: PostExecutionThread
+
+}
